50728a6fa855c722fdbc100ab94210f0155ead70
luisfaria <lfariaxxi@hotmail.com>
2020-05-05 10:08:46 +0100

    iteration 1 to 5, done

--- index.js
+++ index.js
@@ -1,22 +1,45 @@
 class SortedList {
   constructor() {
-    // ...
+    this.items = [];
+    this.length = this.items.length;
   }
 
   add(item) {
-    // ...
+    this.items.push(item);
+    this.items = this.items.sort((a, b) => {
+      if (a > b) {
+        return 1;
+      } else if (a < b) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+    this.length = this.items.length;
   }
 
   load(pos) {
-    // ...
+    if (this.items.length < pos) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
   }
 
   max() {
-    // ...
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.max(...this.items);
+    }
   }
 
   min() {
-    // ...
+    if (this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      return Math.min(...this.items);
+    }
   }
 
   sum() {
